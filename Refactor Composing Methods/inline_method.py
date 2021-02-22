86cf711c7466f4df11e78b9670e898732a9236a7
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-02-21 20:10:25 -0800

    inline method - 2

--- Refactor Composing Methods/inline_method.py
+++ Refactor Composing Methods/inline_method.py
@@ -2,24 +2,19 @@
 # Inline method.
 # TODO: Refactor this program to improve its readability.
 
-LEGAL_DRINKING_AGE = 18
+legal_drinking_age = 18
+
 class Person:
     def __init__(self, my_age):
         self.age = my_age
         
-def enter_night_club(individual):
-    if older_than_18_year_old(individual.age):
+def can_enter_nightclub(self):
+    """Checks Person age against the LEGAL_DRINKING_AGE."""
+    if self.age > legal_drinking_age:
         print("Allowed to enter.")
     else:
         print("Enterance of minors is denited.")
 
-def older_than_18_year_old(age):
-    if age > LEGAL_DRINKING_AGE:
-        return True
-    else: 
-        return False
-    
-    
 person = Person(17.9)
-enter_night_club(person)
+can_enter_nightclub(person)
         
