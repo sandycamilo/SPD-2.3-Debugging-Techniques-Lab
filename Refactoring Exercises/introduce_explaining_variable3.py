0ce301ed12ef8d4db0a3e4206973ed4fbfb6b1fc
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-02-21 18:15:48 -0800

    extract variable- introduce explaining variable 3

--- Refactoring Exercises/introduce_explaining_variable3.py
+++ Refactoring Exercises/introduce_explaining_variable3.py
@@ -1,12 +1,16 @@
 # by Kami Bigdely
 # Extract Variable (alias introduce explaining variable)
-WELL_DONE = 900000
-MEDIUM = 600000
-COOKED_CONSTANT = 0.05
 
-def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
-        return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
+well_done = 900000
+medium = 600000
+cooked_constant = 0.05
+
+def cooking_criteria_satisfied(time, temperature, pressure):
+    well_done_state = 'well-done'
+    medium_state = 'medium'
+    cooked = time * temperature * pressure * cooked_constant
+    if well_done_state and cooked >= well_done:
+        return True 
+    if medium_state and cooked >= medium:
         return True
     return False
